412746f03300345c1d76e1f77c5d126b7bb941f7
desashuno <77dpc77@gmail.com>
2023-06-03 10:01:37 +0200

    second commit

--- push.sh
+++ push.sh
@@ -7,12 +7,12 @@ sudo rm -rv PKGBUILD local_repo out_dir work_dir
 echo 'adding all in the main branch'
 git add --all .
 
-echo 'write your comit comment'
+echo 'write your commit comment'
 
 read input
 
 git commit -m "$input"
 
-git push -f -u master main
+git push -f -u origin main
 
 echo 'done :)'
